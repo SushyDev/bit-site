94b34906d48b782cbf4d629860e94c94f8d70007
SushyDev <xsushy@hotmail.com>
2021-05-26 17:16:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <div class="d-flex align-center">
+                <v-img alt="Bingus Logo" class="shrink mr-2" contain src="./assets/bingus.gif" transition="scale-transition" width="40" />
+            </div>
+        </v-app-bar>
+
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'App',
+
+    data: () => ({
+        //
+    }),
+});
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <v-img :src="require('../assets/dance.gif')" class="my-3" contain height="600" />
+    </div>
+</template>
